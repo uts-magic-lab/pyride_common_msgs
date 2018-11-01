478d227f71c7c665dcd91d820b3f9d74cc8188e0
Xun Wang <wang.xun@gmail.com>
2018-11-01 17:57:45 +1100

    make the router less dumb.

--- scripts/pyride_msg_loopback_router.py
+++ scripts/pyride_msg_loopback_router.py
@@ -2,20 +2,32 @@
 
 import os
 import rospy
+import json
 from pyride_common_msgs.msg import NodeStatus, NodeMessage
 
 class PyRideLoopbackMsgRouter( object ):
     def __init__( self ):
         self.sub = rospy.Subscriber("/pyride/node_message", NodeMessage, self.input_cb)
-        self.pub = rospy.Publisher("/pyride/node_status", NodeStatus,queue_size=2)
+        self.pub = rospy.Publisher("/pyride/node_status", NodeStatus, queue_size=2)
 
     def input_cb( self, input_msg ):
-    	msg = NodeStatus()
-    	msg.node_id = input_msg.node_id
-	msg.header = input_msg.header
-    	msg.status_text = input_msg.command
-    	msg.for_console = False
-    	self.pub.publish( msg )
+        if input_msg.node_id != 'message_router':
+            return
+        try:
+            message = json.loads(input_msg.command)
+        except:
+            rospy.logerr("invalid message format for PyRIDE message router")
+            return
+        if not isinstance(message, dict) or 'node_id' not in message or 'command' not in message:
+            rospy.logerr("invalid message format for PyRIDE message router")
+            return
+
+        msg = NodeStatus()
+        msg.node_id = message['node_id']
+        msg.header = input_msg.header
+        msg.status_text = message['command']
+        msg.for_console = False
+        self.pub.publish( msg )
 
 def main():
     rospy.init_node('pyride_msg_loopback_router')
